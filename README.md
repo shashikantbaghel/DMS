17173e4a406eb2ecc60c752fcb24af9696947325
Shashikant Baghel <shashikant_baghel@thbs.india.com>
2020-05-02 17:09:01 +0530

    setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# DMS
-Dental Management System
---
+++ src/com/dms/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.dms.controller;
+
+import com.dms.view.Home;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		
+		
+		new Home();
+	}
+	
+}
